de9cb0e51f57803de4b2d70b77a2af7fc0d11d5d
Eleanor Lawton <Elawton@emich.edu>
2019-03-21 14:00:56 -0400

    Added connection to Mi/Km -- needs to be tested still

--- app/src/main/java/com/dev2qa/parkedup2/MenuActivity.java
+++ app/src/main/java/com/dev2qa/parkedup2/MenuActivity.java
@@ -3,6 +3,7 @@ package com.dev2qa.parkedup2;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
@@ -12,7 +13,6 @@ import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
 public class MenuActivity extends FragmentActivity {
-    static Boolean miles = true;
     static Boolean notify = true;
 
     private LocationManager locMng = new LocationManager();
@@ -32,17 +32,49 @@ public class MenuActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        MIKI = (ToggleButton) findViewById(R.id.MIKItoggle);
+        final SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        boolean MiKi = preferences.getBoolean("MiKi", true);  //default is true
 
-        MIKI.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    miles = false;
+        //MIKI.setChecked(MiKi);
+
+        MIKI = (ToggleButton) findViewById(R.id.MIKItoggle);
+//        MIKI.setOnClickListener(new ToggleButton.OnClickListener() {
+//
+//            public void onClick(View v) {
+//                SharedPreferences sharedPreferences = PreferenceManager
+//                        .getDefaultSharedPreferences(getApplicationContext());
+//                SharedPreferences.Editor editor = sharedPreferences.edit();
+//                editor.putBoolean("toggleButton", MIKI.isChecked());
+//                editor.commit();
+//            }
+//        });
+
+        MIKI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((MIKI.isChecked())) {
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("Miles", true); // value to store
+                    editor.commit();
                 } else {
-                    // The toggle is disabled
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("Kilometers", false); // value to store
+                    editor.commit();
                 }
+                LocationManager lm = new LocationManager();
+                lm.toggleUnits();
             }
         });
+
+//        MIKI.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    miles = false;
+//                } else {
+//                    // The toggle is disabled
+//                }
+//            }
+//        });
 
         Notify = (ToggleButton) findViewById(R.id.notify);
         Notify.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -75,9 +107,6 @@ public class MenuActivity extends FragmentActivity {
                 }
             }
         });
-    }
-    public static boolean getMiles(){
-        return miles;
     }
     public static boolean getNotify(){
         return notify;
