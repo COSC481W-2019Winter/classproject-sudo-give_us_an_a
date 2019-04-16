809f5506b2aeb7a97069006bb3b4fb025cc435fd
Brian Mikolajczyk <bmikolaj@emich.edu>
2019-04-16 17:09:35 -0400

    Re-added NotificationChan and ForegroundService

---
+++ app/src/main/java/com/dev2qa/parkedup2/NotificationChan.java
@@ -0,0 +1,30 @@
+package com.dev2qa.parkedup2;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class NotificationChan extends Application {
+    public static final String CHANNEL_ID = "theServiceChannel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "The Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+}
